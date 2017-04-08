f96991e45f07a8276fa6065783d3dc376e51201d
Andy Wong <wongandy97@gmail.com>
2017-04-08 18:33:25 -0400

    Fix bottom border issue with border color being off

--- Don'tStressIt/StudentUserViewController.swift
+++ Don'tStressIt/StudentUserViewController.swift
@@ -19,7 +19,7 @@ class StudentUserViewController: UIViewController{
 	override func viewDidLayoutSubviews() {
 		//Top TextFieldLine
 		let topLayer = CALayer()
-		let borderWidth = CGFloat(2.0)
+		let borderWidth = CGFloat(0.3)
 		topLayer.borderColor = UIColor.gray.cgColor
 		topLayer.frame = CGRect(x:0, y:0, width: nameTextField.frame.width, height: 1)
 		topLayer.borderWidth = borderWidth
@@ -30,7 +30,7 @@ class StudentUserViewController: UIViewController{
 		//Bottom TextFieldLine
 		let bottomLayer = CALayer()
 		bottomLayer.borderColor = UIColor.gray.cgColor
-		bottomLayer.frame = CGRect(x:0, y:nameTextField.frame.height-1.0, width: nameTextField.frame.width, height: nameTextField.frame.height - 1.0)
+		bottomLayer.frame = CGRect(x:0, y: nameTextField.frame.height-1.0, width: nameTextField.frame.width, height: 1)
 		bottomLayer.borderWidth = borderWidth
 		nameTextField.layer.addSublayer(bottomLayer)
 		nameTextField.layer.masksToBounds = true

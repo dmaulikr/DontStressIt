d039a87888ac0f53371dafe91c26268c3be4c3cb
Andy Wong <wongandy97@gmail.com>
2017-04-08 18:39:18 -0400

    Remove textViews, replaced with placeholder text for better view

--- Don'tStressIt/StudentUserViewController.swift
+++ Don'tStressIt/StudentUserViewController.swift
@@ -12,19 +12,22 @@ import UIKit
 class StudentUserViewController: UIViewController{
 	
 	@IBOutlet weak var nameTextField: UITextField!
+	@IBOutlet weak var usernameTextField: UITextField!
+	@IBOutlet weak var passwordTextField: UITextField!
+	@IBOutlet weak var confirmTextField: UITextField!
 	override func viewDidLoad() {
 
 	}
 	
 	override func viewDidLayoutSubviews() {
-		//Top TextFieldLine
-		let topLayer = CALayer()
+//		//Top TextFieldLine
+//		let topLayer = CALayer()
 		let borderWidth = CGFloat(0.3)
-		topLayer.borderColor = UIColor.gray.cgColor
-		topLayer.frame = CGRect(x:0, y:0, width: nameTextField.frame.width, height: 1)
-		topLayer.borderWidth = borderWidth
-		nameTextField.layer.addSublayer(topLayer)
-		nameTextField.layer.masksToBounds = true
+//		topLayer.borderColor = UIColor.gray.cgColor
+//		topLayer.frame = CGRect(x:0, y:0, width: nameTextField.frame.width, height: 1)
+//		topLayer.borderWidth = borderWidth
+//		nameTextField.layer.addSublayer(topLayer)
+//		nameTextField.layer.masksToBounds = true
 		
 		
 		//Bottom TextFieldLine
@@ -32,7 +35,12 @@ class StudentUserViewController: UIViewController{
 		bottomLayer.borderColor = UIColor.gray.cgColor
 		bottomLayer.frame = CGRect(x:0, y: nameTextField.frame.height-1.0, width: nameTextField.frame.width, height: 1)
 		bottomLayer.borderWidth = borderWidth
+		
 		nameTextField.layer.addSublayer(bottomLayer)
 		nameTextField.layer.masksToBounds = true
+		
+		usernameTextField.layer.addSublayer(bottomLayer)
+		usernameTextField.layer.masksToBounds = true
+		
 	}
 }

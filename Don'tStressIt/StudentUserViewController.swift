d924594860d3a01d4739429cba61f12da4844d5a
Andy Wong <wongandy97@gmail.com>
2017-04-08 19:04:15 -0400

    Add border lines to textfield to give a better look, add function for easier calling

--- Don'tStressIt/StudentUserViewController.swift
+++ Don'tStressIt/StudentUserViewController.swift
@@ -20,27 +20,32 @@ class StudentUserViewController: UIViewController{
 	}
 	
 	override func viewDidLayoutSubviews() {
-//		//Top TextFieldLine
-//		let topLayer = CALayer()
-		let borderWidth = CGFloat(0.3)
-//		topLayer.borderColor = UIColor.gray.cgColor
-//		topLayer.frame = CGRect(x:0, y:0, width: nameTextField.frame.width, height: 1)
-//		topLayer.borderWidth = borderWidth
-//		nameTextField.layer.addSublayer(topLayer)
-//		nameTextField.layer.masksToBounds = true
-		
-		
-		//Bottom TextFieldLine
-		let bottomLayer = CALayer()
-		bottomLayer.borderColor = UIColor.gray.cgColor
-		bottomLayer.frame = CGRect(x:0, y: nameTextField.frame.height-1.0, width: nameTextField.frame.width, height: 1)
-		bottomLayer.borderWidth = borderWidth
-		
-		nameTextField.layer.addSublayer(bottomLayer)
+		//Top TextFieldLine
+		let topLayer = CALayer()
+		let borderWidth = CGFloat(1.3)
+		topLayer.borderColor = UIColor.gray.cgColor
+		topLayer.frame = CGRect(x:0, y:0, width: nameTextField.frame.width, height: 1)
+		topLayer.borderWidth = borderWidth
+		nameTextField.layer.addSublayer(topLayer)
 		nameTextField.layer.masksToBounds = true
 		
-		usernameTextField.layer.addSublayer(bottomLayer)
-		usernameTextField.layer.masksToBounds = true
 		
+		//Bottom TextFieldLines
+		configureTextField (x: 0, y: nameTextField.frame.height-1, width: nameTextField.frame.width, height: 1.0, textField: nameTextField)
+		configureTextField (x: 0, y: usernameTextField.frame.height-1, width: usernameTextField.frame.width, height: 1.0, textField: usernameTextField)
+		configureTextField (x: 0, y: passwordTextField.frame.height-1, width: passwordTextField.frame.width, height: 1.0, textField: passwordTextField)
+		configureTextField (x: 0, y: confirmTextField.frame.height-1, width: confirmTextField.frame.width, height: 1.0, textField: confirmTextField)
+		
+	}
+	
+	func configureTextField (x:CGFloat, y:CGFloat ,width:CGFloat, height:CGFloat, textField:UITextField){
+		let bottomLayer = CALayer()
+		let borderWidth = CGFloat(1.3)
+		bottomLayer.borderColor = UIColor.gray.cgColor
+		bottomLayer.frame = CGRect(x:x, y: y, width: width, height: height)
+		textField.borderStyle = UITextBorderStyle.none
+		bottomLayer.borderWidth = borderWidth
+		textField.layer.addSublayer(bottomLayer)
+		textField.layer.masksToBounds = true
 	}
 }
